8e282a71669a4e27a6b27608d564ffcc1db1b101
Nickolas <nickolasiaquinta@gmail.com>
2021-04-21 17:46:43 -0400

    Fixing modkey to be Windows Key

--- install.sh
+++ install.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-
-cp -rf suckless/ ~/.suckless
+sudo cp dwm.desktop /usr/share/xsessions/
+cp startup.sh ~/.startup.sh
+cp xsession ~/.xsession
+cp -r suckless/ ~/.suckless
 cd ~/.suckless/dmenu/
 sudo make clean install
 cd ../dwm/
 sudo make clean install
 cd ../dwmblocks/
 sudo make clean install
-sudo cp dwm.desptop /usr/share/xsessions/
-cp startup.sh ~/.startup.sh
-cp xsession ~/.xsession
