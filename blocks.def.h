0209d12057c82d00b6c7d9d43fc43e1727fb21ce
Nickolas <nickolasiaquinta@gmail.com>
2021-11-17 16:43:26 -0500

    Adding date to blocks.def.h

--- blocks.def.h
+++ blocks.def.h
@@ -1,13 +1,15 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"BAT:", "cat /sys/class/power_supply/BAT0/capacity", 30, 0},
+    {"BAT:", "cat /sys/class/power_supply/BAT0/capacity", 30, 0},
 
-	{"", "cat /sys/class/power_supply/BAT0/status", 5, 0},
+    {"", "cat /sys/class/power_supply/BAT0/status", 5, 0},
 
-	{"MEM:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g", 30, 0},
+    {"MEM:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g", 30, 0},
 
-	{"", "date '+%H:%M'", 1, 0},
+    {"", "date '+%d/%m/%y'", 0, 0},
+
+    {"", "date '+%H:%M'", 1, 0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
