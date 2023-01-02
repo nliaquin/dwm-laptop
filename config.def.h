25ec5a6b16258bc34d5e85d8a242f9b1a61ff014
Nickolas <nickolasiaquinta@gmail.com>
2023-01-02 18:23:28 -0500

    changing crimson color to Splinter Cell 3 green

--- config.def.h
+++ config.def.h
@@ -11,11 +11,11 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_crimson[]        = "#a30306";
+static const char col_scc3green[]   = "#008000";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_crimson,  col_crimson  },
+	[SchemeSel]  = { col_gray4, col_scc3green,  col_scc3green  },
 };
 
 /* tagging */
@@ -56,7 +56,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_crimson, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_scc3green, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 
 static const Key keys[] = {
